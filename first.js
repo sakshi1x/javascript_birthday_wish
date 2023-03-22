030f90c61a694d7a8c558cf4a0c286f93b98c4ee
sakshi1x <nepalsakshi05@gmail.com>
2023-03-22 16:14:36 +0545

    initial commit

---
+++ first.js
@@ -0,0 +1,8 @@
+function myfunction() {
+    document.getElementById("one").style.visibility = "visible";
+}
+function move_img() {
+document.getElementById("tafel").style.visibility = "visible";
+
+}
+
